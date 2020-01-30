78152f76779e155fb5617fff974c6e061fcdef94
Grayson Mechler <Graysonmechler@gmail.com>
2020-01-29 21:20:03 -0600

    orders in prog

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
 INSERT INTO Inventories
-    (item, qty, unit, createdAt, updatedAt)
+    (item, qty, unit, createdAt, updatedAt, critical)
 VALUES
-    ('tomato', 10, 'each', '2020-01-27 12:18:01', '2020-01-27 01:18:01');
+    ('tomato', 10, 'each', '2020-01-27 12:18:01', '2020-01-27 01:18:01', 5);
 INSERT INTO Inventories
-    (item, qty, unit, createdAt, updatedAt)
+    (item, qty, unit, createdAt, updatedAt, critical)
 VALUES
-    ('POTATO', 10, 'each', '2020-01-27 01:18:01', '2020-01-27 01:18:01');
+    ('POTATO', 10, 'each', '2020-01-27 01:18:01', '2020-01-27 01:18:01', 5);
 INSERT INTO Inventories
-    (item, qty, unit, createdAt, updatedAt)
+    (item, qty, unit, createdAt, updatedAt, critical)
 VALUES
-    ('FLOUR', 10, 'lbs', '2020-01-27 01:18:01', '2020-01-27 01:18:01');
+    ('FLOUR', 10, 'lbs', '2020-01-27 01:18:01', '2020-01-27 01:18:01', 5);
