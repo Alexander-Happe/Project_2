5cf0f8ee5fde85016d09ed567646037d9709e2b9
Jeremy Pellow <jeremy.pellow@gmail.com>
2020-01-29 19:57:33 -0600

    added api get request to the receiving files, also added html

--- routes/inv-api-routes.js
+++ routes/inv-api-routes.js
@@ -1,10 +1,20 @@
 var db = require("../models");
+var axios = require("axios")
 
 module.exports = function (app) {
     app.get("/api/inventory", function (req, res) {
         db.Inventory.findAll({}).then(function (dbInv) {
             res.json(dbInv)
         });
+    });
+    app.get("/api/receive", function (req, res) {
+        axios({
+            url: 'https://api.upcitemdb.com/prod/trial/lookup?upc=012000811203',
+            method: 'get'
+        }).then((response) => {
+            res.json(response)
+        });
+        //axios call to url promise to get data back and send back res.json
     });
     app.get("/api/critical", function (req, res) {
         db.Inventory.findAll({
