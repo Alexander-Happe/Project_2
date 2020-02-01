60825bc609725abc7b18a8c3d819a80645418651
Alex Happe <abhdiver1994@gmail.com>
2020-02-01 10:47:02 -0600

    makes tables and buttons for the menu items

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -5,9 +5,11 @@ $(document).ready(function() {
 
   function getMenuItems() {
     $.get("/api/recipes", function(data) {
-      var rowsToAdd = [];
-      for (var i = 0; i < data.length; i++) {
-        rowsToAdd.push(data[i].name);
+      for (var i = 0; i < 0; i++) {
+        var menuItem = `<tr class="toollow><td> ${data[i].name} </td>
+          <td><a class='make-item'>Make Item</a></td>
+          <td><a class='delete-item'>Delete Item</a></td>`;
+        menuList.append(menuItem);
       }
     }).then(function(data) {
       for (var i = 0; i < data.length; i++) {
