9bdc050c2a95a83410d90ef3ee77dfdfda1bbf07
Alex Happe <abhdiver1994@gmail.com>
2020-02-03 17:46:42 -0600

    need the new stuff

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
   var menuList = $(".menu-list");
 
-  getMenuItems();
+  $(document).on("click", ".delete-item", function() {
+    var listItemData = $(this).data("id");
+    $.ajax({
+      method: "DELETE",
+      url: "/api/recipes/" + listItemData
+    }).then(makeList);
+  });
 
-  function getMenuItems() {
+  makeList();
+
+  function makeList() {
     $.get("/api/recipes", function(data) {
-      for (var i = 0; i < 0; i++) {
-        var menuItem = `<tr class="toollow><td> ${data[i].name} </td>
-          <td><a class='make-item'>Make Item</a></td>
-          <td><a class='delete-item'>Delete Item</a></td>`;
+      for (var i = 0; i < data.length; i++) {
+        var menuItem = `<tr data-name='${data[i]}'><td> ${data[i].name} </td>
+      <td><a class='make-item' data-id='${data[i].name}'>Make Item</a></td>
+      <td><a class='delete-item' data-id='${data[i].id}'>Delete Item</a></td></tr>`;
         menuList.append(menuItem);
       }
-    }).then(function(data) {
-      for (var i = 0; i < data.length; i++) {
-        newMenuItem = $("<li>");
-        newMenuItem.text(data[i].name);
-        menuList.append(newMenuItem);
-      }
+      menuItem = ``;
     });
   }
 });
