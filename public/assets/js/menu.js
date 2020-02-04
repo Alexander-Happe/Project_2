9ac896296beaefa97ed4b1409a6b944b7ff56cb4
Jeremy Pellow <jeremy.pellow@gmail.com>
2020-02-03 21:20:19 -0600

    added recieve functionality to add units to the DB

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,25 +1,3 @@
-// $(document).ready(function() {
-//   var menuList = $(".menu-list");
-
-//   getMenuItems();
-
-//   function getMenuItems() {
-//     $.get("/api/recipes", function(data) {
-//       var rowsToAdd = [];
-//       for (var i = 0; i < data.length; i++) {
-//         rowsToAdd.push(data[i].name);
-//       }
-//     }).then(function(data) {
-//       for (var i = 0; i < data.length; i++) {
-//         newMenuItem = $("<li>");
-//         newMenuItem.text(data[i].name);
-//         menuList.append(newMenuItem);
-//       }
-//     });
-//   }
-// });
-
-
 $(document).ready(function () {
   var menuList = $(".menu-list");
 
@@ -41,4 +19,5 @@ $(document).ready(function () {
       }
     });
   }
+
 });
