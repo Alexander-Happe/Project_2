dec1921dd37ad12f8c47112d38c105fe0090f8ff
Grayson Mechler <Graysonmechler@gmail.com>
2020-02-03 21:08:24 -0600

    setting up pdfgen file

--- public/assets/js/orders.js
+++ public/assets/js/orders.js
@@ -1,20 +1,4 @@
-$(document).ready(function () {
-  var criticalItemField = $("#criticalItemField");
-  ListCritical();
-  // Function to list all ingredients critically low
-  function ListCritical() {
-    $.get("/api/inventory", function (data) {
-      for (var i = 0; i < data.length; i++) {
-        if (data[i].isCritical === true) {
-          var criticalItem = `<tr class="toolow"><td> ${data[i].item} </td> 
-          <td> ${data[i].qty} </td>
-          <td> ${data[i].unit} </td>`;
-          criticalItemField.append(criticalItem);
-        }
-      }
-    });
-  }
-
+$(document).ready(function() {
   function addToList() {
     var listItem = document.getElementById("#newItemInput");
     console.log(listItem);
