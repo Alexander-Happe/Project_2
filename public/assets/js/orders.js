ab142619a857a8b7e36a2f889bae7ab516158ebc
Grayson Mechler <Graysonmechler@gmail.com>
2020-02-04 20:26:46 -0600

    finding error in code

--- public/assets/js/orders.js
+++ public/assets/js/orders.js
@@ -9,6 +9,33 @@ $(document).ready(function() {
     rmBtn.click();
   }
 
+  function showStorage() {
+    var values = [],
+      keys = Object.keys(localStorage),
+      i = keys.length;
+    while (i--) {
+      values.push(localStorage.getItem(keys[i]));
+    }
+    for (j = 0; j < values.length; j++) {
+      var newTr = $("<div>").attr("class", "columns is-mobile shopitem");
+      var newListItem = $("<li>")
+        .attr(
+          "class",
+          "listItems column is-four-fifths-desktop is-two-thirds-tablet is-two-thirds-mobile"
+        )
+        .text(values[j]);
+      var addSpan = $("<button>")
+        .attr(
+          "class",
+          "column is-2-desktop is-2-tablet is-2-mobile listButton close"
+        )
+        .text("X");
+      newTr.append(newListItem, addSpan);
+      $("#orderItemsDisplay").append(newTr);
+    }
+  }
+  showStorage();
+
   var critList = localStorage.length;
   console.log(critList);
 });
