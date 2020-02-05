bbc841557507e62103d0d16e3a35606f2aa1f0e0
Alex Happe <abhdiver1994@gmail.com>
2020-02-04 21:11:47 -0600

    what is happening

--- routes/inv-api-routes.js
+++ routes/inv-api-routes.js
@@ -1,45 +1,17 @@
 var db = require("../models");
 var axios = require("axios");
 
-<<<<<<< HEAD
 module.exports = function(app) {
+  //
   app.get("/api/inventory", function(req, res) {
     db.Inventory.findAll({}).then(function(dbInv) {
       res.json(dbInv);
     });
-    app.get("/api/receive/:upc", function(req, res) {
-      axios({
-        url:
-          "https://api.upcitemdb.com/prod/trial/lookup?upc=" + req.params.upc,
-        method: "get"
-      }).then(response => {
-        console.log(response);
-        res.json(response.data);
-      });
-      //axios call to url promise to get data back and send back res.json
-    });
-    app.get("/api/critical", function(req, res) {
-      db.Inventory.findAll({
-        where: { isCritical: true }
-      }).then(function(dbInv) {
-        res.json(dbInv);
-      });
-    });
-  });
-  app.post("/api/inventory", function(req, res) {
-=======
-module.exports = function (app) {
-  //
-  app.get("/api/inventory", function (req, res) {
-    db.Inventory.findAll({}).then(function (dbInv) {
-      res.json(dbInv);
-    });
   });
   //GET request to send API a UPC and get back the JSON data
-  app.get("/api/receive/:upc", function (req, res) {
+  app.get("/api/receive/:upc", function(req, res) {
     axios({
-      url:
-        "https://api.upcitemdb.com/prod/trial/lookup?upc=" + req.params.upc,
+      url: "https://api.upcitemdb.com/prod/trial/lookup?upc=" + req.params.upc,
       method: "get"
     }).then(response => {
       console.log(response);
@@ -47,16 +19,15 @@ module.exports = function (app) {
     });
   });
   //GET request to find all "Critial" inventory so it can be displayed on the inventory screen
-  app.get("/api/critical", function (req, res) {
+  app.get("/api/critical", function(req, res) {
     db.Inventory.findAll({
       where: { isCritical: true }
-    }).then(function (dbInv) {
+    }).then(function(dbInv) {
       res.json(dbInv);
     });
   });
   //POST request to add new items to the inventory
-  app.post("/api/inventory", function (req, res) {
->>>>>>> 41433495405377ba863670878a0b315ed4a09191
+  app.post("/api/inventory", function(req, res) {
     db.Inventory.create({
       item: req.body.item,
       qty: req.body.qty,
@@ -67,23 +38,8 @@ module.exports = function (app) {
       res.json(dbInv);
     });
   });
-<<<<<<< HEAD
-
-  app.get("/api/inventory/:id", function(req, res) {
-    db.Inventory.findAll({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbInv) {
-      res.json(dbInv);
-    });
-  });
-
-  app.put("/api/inventory/:id", function(req, res) {
-=======
   //PUT request to update the quantity of an inventory item then update its "critial" status if the qty is no longer below the threshold
-  app.put("/api/inventory/:id", function (req, res) {
->>>>>>> 41433495405377ba863670878a0b315ed4a09191
+  app.put("/api/inventory/:id", function(req, res) {
     db.Inventory.update(
       {
         qty: req.body.qty
@@ -110,12 +66,8 @@ module.exports = function (app) {
       });
     });
   });
-<<<<<<< HEAD
-  app.put("/api/inventory/:id", function(req, res) {
-=======
 
-  app.put("/api/inventory/:id", function (req, res) {
->>>>>>> 41433495405377ba863670878a0b315ed4a09191
+  app.put("/api/inventory/:id", function(req, res) {
     db.Inventory.update(
       {
         isCritical: false
